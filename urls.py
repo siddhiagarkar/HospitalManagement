88e48c553c81a56e7b6e36adb52b1d1ad703cfcb
siddhiagarkar <siddhiagarkar1203@gmail.com>
2022-07-16 11:35:53 +0530

    register page done

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ from .forms import authform
 
 urlpatterns=[
     path('', views.index, name='homepg'),
-
+    path('loginpg', include('django.contrib.auth.urls')),
     path('regpg', views.registrationpage, name='regpg'),
     path('loginpg',auth_views.LoginView.as_view(template_name = "hosp/loginpage.html", authentication_form = authform), name = "LF"),
 ]
